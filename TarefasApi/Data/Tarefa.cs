8981ab10003203806ee1d237a866ab8fbbe075c4
GuilhermeSouza <guilhermebencici@gmail.com>
2023-11-13 08:55:26 -0300

    Minimal API with DAPPER

---
+++ TarefasApi/Data/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace TarefasApi.Data
+{
+    public class Tarefa
+    {
+        [Table("Tarefas")]
+        public record Task(int Id, string Atividade, string Status);
+    }
+}
