9fac5fa8cd5a8ac6d0e90b3ee3b7bbf0f11c412e
GuilhermeSouza <guilhermebencici@gmail.com>
2023-12-21 18:02:27 -0300

    Criando Repositorys

---
+++ APICatalogo/APICatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using System.Collections.Generic;
+
+namespace APICatalogo.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(AppDbContext contexto) : base(contexto)
+        {
+        }
+
+        public IEnumerable<Categoria> GetCategoriasProdutos()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace APICatalogo.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected AppDbContext _context;
+
+        // CONSTRUTOR
+        public Repository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        // RETORNA UMA LISTA DE ENTIDADES
+        public IQueryable<T> Get()
+        {
+            return _context.Set<T>().AsNoTracking();// DESABILITANDO O RASTREAMENTO DE ENTIDADES (ASNOTRACKING)
+        }
+        //RETORNANDO UMA ENTIDADE
+        public T GetById(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().SingleOrDefault(predicate);// PREDICATE VALIDA O CRITÉRIO
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;// INFORMANDO O CONTEXTO QUE A ENTIDADE FOI ALTERADA
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
